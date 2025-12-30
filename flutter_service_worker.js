8424e3f63cf28db4f68b8b554be5ac677a2cf8b2
choisang <chltkd0311@gmail.com>
2025-12-31 01:44:44 +0900

    Fix text quiz countdown slider to match image quiz behavior

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -71,14 +71,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "ac8125013588908b6a7fc0de83e99165
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "b3dc34ef3c26374a4e29b016a06ad9c7",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "1f9786b771df2b632be91b0b6c859f7a",
+"flutter_bootstrap.js": "cab65af849114bde6981a39e59428b66",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "37f66f1faa24876146ecb066db82eee4",
 "/": "37f66f1faa24876146ecb066db82eee4",
-"main.dart.js": "9d98223e326fd6fbc99015fde12f9aac",
+"main.dart.js": "fe28545df1b57b8d763813c1700ee80f",
 "manifest.json": "2c20f13b245992f799d22727b42ba171",
 "version.json": "ed6224453623bebf18b3d66a21f60759"};
 // The application shell files that are downloaded before a service worker can
